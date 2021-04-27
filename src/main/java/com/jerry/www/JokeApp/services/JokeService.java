28a376104620c5af2c3a9f06ee15bfc3c09e0253
JerryDenzinger <gerardo_denzinger@outlook.de>
2021-04-27 13:23:08 +0200

    Correction in structure

---
+++ src/main/java/com/jerry/www/JokeApp/services/JokeService.java
@@ -0,0 +1,6 @@
+package com.jerry.www.JokeApp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
--- src/main/java/services/JokeService.java
+++
@@ -1,5 +0,0 @@
-package services;
-
-public interface JokeService {
-	String getJoke();
-}
